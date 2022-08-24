3aa8814dc7dc048133b95d4a9af79aeaa0438d9d
DanMundy <dan@mundy.co>
2022-08-24 12:44:09 +0930

    No comment

--- DM-OneDrive.ps1
+++ DM-OneDrive.ps1
@@ -6,9 +6,11 @@
 # Function: Get-DMOneDriveSites
 # Purpose:  Show OneDrive sites
 # Requires: Install-DM-Module -Name PnP.PowerShell
-# Usage: Get-DMOneDriveSites --tenant "companyname" --outFile "C:\DM\Results.csv"
+# Usage: Get-DMOneDriveSites --tenant "mundy" --outFile "C:\DM\Results.csv"
+#        Get-DMOneDriveSites -tenant "mundy" | sort UsageInMB -Descending
 # 
-# (if eg SharePoint URL is "https://companyname.sharepoint.com" then Tenant is "companyname")
+# (if eg SharePoint URL is "https://mundy.sharepoint.com" then Tenant is "mundy")
+# Todo: Parameter checking, to make sure it's just a single word, no "https://" included (as I'm always mistyping it)
 
 function Get-DMOneDriveSites ($tenant, $outFile) { 
     if ($(get-pnpcontext).url -ne "https://$tenant.sharepoint.com") { Connect-DM-SPSite -Url "https://$tenant.sharepoint.com" }
@@ -21,6 +23,19 @@ function Get-DMOneDriveSites ($tenant, $outFile) {
     } Else {
         $Result | Select Title, URL, @{L='Modified';E={$_.LastContentModifiedDate}}, @{L='UsageInMB';E={$_.StorageUsageCurrent}}
     }
+}
+
+# Usage:   Get-DM-OneDriveSitesFileCount -SiteURL "https://purewineco-my.sharepoint.com/personal/stanley_tan_purewine_co"
+# Sources: https://dm.wtf/RFGK https://dm.wtf/XIHN
+# Todo:    Combine it with the above, so it scans *all* OneDrives, and shows both usage in MB, and number of files
+
+function Get-DM-OneDriveSitesFileCount ($SiteURL) {
+    $ListName = "Documents"
+
+    Connect-PnPOnline $SiteURL -Interactive
+    $List = Get-PnPList -Identity $ListName
+
+    Get-PnPList -Identity $ListName | Select-Object  ParentWebUrl,Title,ItemCount 
 }
 
 ## ----------------------------------------------------------------------------
@@ -71,5 +86,29 @@ Function Get-DMSmbSharePaths {
         $Shares | Select __Server,Name,Path,Description | Export-Csv -NoTypeInformation -Path "$OutFile" -Append
     } Else {
         $Shares | Select __Server,Name,Path,Description
+    }
+}
+
+## ----------------------------------------------------------------------------
+
+# Function: Grant-DM-AdminAccessToAllOneDriveSites
+# Purpose:  
+# Requires: Install-DM-Module -Name PnP.PowerShell 
+# Usage: Grant-DM-AdminAccessToAllOneDriveSites -AdminSiteURL "https://crescent-admin.sharepoint.com" -SiteCollAdmin "sarazhak@crescent.com"
+
+Function Grant-DM-AdminAccessToAllOneDriveSites ($AdminSiteURL,$SiteCollAdmin) {
+      
+    #Connect to PnP Online to the Tenant Admin Site
+    Connect-PnPOnline -Url $AdminSiteURL -Interactive
+      
+    #Get All OneDrive Sites
+    $OneDriveSites = Get-PnPTenantSite -IncludeOneDriveSites -Filter "Url -like '-my.sharepoint.com/personal/'"
+     
+    #Loop through each site
+    ForEach($Site in $OneDriveSites)
+    { 
+        #Add Site collection Admin
+        Set-PnPTenantSite -Url $Site.URL -Owners $SiteCollAdmin
+        Write-Host -f Green "Added Site Collection Admin to: "$Site.URL
     }
 }
