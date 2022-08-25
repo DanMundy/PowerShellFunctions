436d6757bc8d8664bcb908833a176471c2fceea6
Dan <dan@mundy.co>
2022-08-25 15:38:40 +0930

    Delete Download-DM-PowerShell-Functions.ps1

--- Download-DM-PowerShell-Functions.ps1
+++
@@ -1,6 +0,0 @@
-function Get-DMPowerShellFunctions {
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    wget https://dm.wtf/psf -outFile dm.zip
-    Expand-Archive .\dm.zip . -Force
-}
-Get-DMPowerShellFunctions
